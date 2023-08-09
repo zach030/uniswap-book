59643d2e3e2b49c4b8fe59b5eb8b325fccda7b83
zach <zachchou016@gmail.com>
2023-08-09 09:08:18 +0800

    init v1 factory

--- src/uniswap-v1/Exchange.sol
+++ src/uniswap-v1/Exchange.sol
@@ -1,8 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {IFactory} from "./Interface.sol";
+
 contract Exchange {
+    bytes32 public name;
+    bytes32 public symbol;
+    uint256 public decimals;
+    address public token;
+    IFactory public factory;
+
     constructor() {
         
+    }
+
+    function setup(address tokenAddr) public{
+        require(address(factory)==address(0) && token==address(0), "");
+        factory = IFactory(msg.sender);
+        token = tokenAddr;
+        name = 0x556e697377617020563100000000000000000000000000000000000000000000;
+        symbol = 0x554e492d56310000000000000000000000000000000000000000000000000000;
+        decimals = 18;
     }
 }
--- src/uniswap-v1/Factory.sol
+++ src/uniswap-v1/Factory.sol
@@ -1,8 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./Exchange.sol";
+
 contract Factory {
+    address public exchangeTemplate;
+    uint256 public tokenCount;
+    mapping (address => address) tokenToExchange;
+    mapping (address => address) exchangeToToken;
+    mapping (uint256 => address) idToToken;
+
+    event NewExchange(address token, address exchange);
+
     constructor() {
         
+    }
+
+    function initialFactory(address template) public {
+        require(exchangeTemplate == address(0), "template address not zero");
+        require(template != address(0), "empty template address");
+        exchangeTemplate = template;
+    }
+
+    function createExchange(address token) public returns(address){
+        require(token != address(0), "token zero address");
+        require(exchangeTemplate != address(0), "exchange template zero address");
+        require(tokenToExchange[token] == address(0), "exchange address not zero");
+        address exchangeAddr = create_with_code_of(exchangeTemplate);
+        Exchange exchange = Exchange(exchangeAddr);
+        exchange.setup(token);
+        tokenToExchange[token] = exchangeAddr;
+        exchangeToToken[exchangeAddr] = token;
+        uint256 tokenId = tokenCount + 1;
+        tokenCount = tokenId;
+        idToToken[tokenId] = token;
+        emit NewExchange(exchangeAddr, token);
+        return exchangeAddr;
+    }
+
+    function getExchange(address token) public view returns (address) {
+        return tokenToExchange[token];
+    }
+
+    function getToken(address exchange) public view returns (address) {
+        return exchangeToToken[exchange];
+    }
+
+    function getTokenWithId(uint256 tokenId) public view returns (address) {
+        return idToToken[tokenId];
+    }
+
+    function create_with_code_of(address contractTemplate) internal returns (address newContract) {
+        bytes memory code = type(Exchange).creationCode;
+        assembly {
+            newContract := create(0, add(code, 0x20), mload(code))
+        }
     }
 }
